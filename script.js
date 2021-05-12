3b08ae50c609376a6958564bf98f48887099a2c9
e.antonov <zhekasudarium@gmail.com>
2021-05-12 17:32:30 +0300

    add script

---
+++ script.js
@@ -0,0 +1,30 @@
+let str = '   foo  ';
+
+console.log(str.length); // 8
+
+str = str.trimLeft(); //Реализуйте функцию ltrim, которая будет убирать пробелы слева. 
+console.log(str.length); // 5
+console.log(str);
+
+
+let str_2 = 'foo     ';
+
+console.log(str_2.length); // 8
+
+str_2 = str_2.trimRight(); //Реализуйте функцию Rtrim, которая будет убирать пробелы справа. 
+console.log(str_2.length); // 3
+console.log(str_2);
+
+
+
+let str_3 = '   foo    ';
+
+console.log(str_3.length); // 8
+
+str = str_3.trim(); //Реализуйте функцию trim, которая будет убирать пробелы по концам строки.
+console.log(str_3.length); // 10
+console.log(str_3);
+
+
+
+
